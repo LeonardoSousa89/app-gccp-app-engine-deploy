89acd4fa9e40e698b35ceea7ed6d476ce8f032fb
Mendes Sousa <leoprofessionallogytech@outlook.com>
2022-08-15 18:29:00 -0300

    sass initial config

--- index.js
+++ index.js
@@ -7,5 +7,4 @@ app.use('/static', express.static('public'))
 app.use('/',server)
 
 app.listen(process.env.PORT  || port)
-//localhost:8081
-//http://localhost:8081/static/index.scss
+
---
+++ public/components/index.js
@@ -0,0 +1,1 @@
+const _DOC=document
