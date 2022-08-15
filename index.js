a2fbd3f6387f033c0e9f6a9756ed24d18f0f39fb
Mendes Sousa <leoprofessionallogytech@outlook.com>
2022-08-15 18:10:54 -0300

    insercao sass file,express.static

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const server=require('./views/server')
 const express=require('express')
 const app=express()
 
+app.use('/static', express.static('public'))
 app.use('/',server)
 
 app.listen(process.env.PORT  || port)
+//localhost:8081
+//http://localhost:8081/static/index.scss
